6210b1c04d2a405c87e4d9c286064cdaf4bccdeb
Spitfire <adanayaztracer@gmail.com>
2024-07-24 21:57:58 +0500

    Add Meta Tags
    Signed-off-by: Spitfire <adanayaztracer@gmail.com>

--- frontend/src/routes/(landing)/+page.svelte
+++ frontend/src/routes/(landing)/+page.svelte
@@ -7,8 +7,40 @@
 	import TeamSection from '../../components/landing/index/team-section.svelte';
 	import ThirdSection from '../../components/landing/index/third-section.svelte';
 	import WaitlistRegisterSection from '../../components/landing/index/waitlist-register-section.svelte';
+	import { MetaTags } from 'svelte-meta-tags';
 </script>
 
+<MetaTags
+	title="Gamebuddies | By Luceforge Studio"
+	titleTemplate="%s - GB Spitfire"
+	description="Connect with other gamebuddies, team up on the spot and start dealing with business! Don't wait for anyone. Matchmake, Converse & Play!"
+	canonical="https://gamebuddies.vercel.app"
+	openGraph={{
+		url: 'https://gamebuddies.vercel.app',
+		title: 'Gamebuddies | By Luceforge Studio',
+		description:
+			"Connect with other gamebuddies, team up on the spot and start dealing with business! Don't wait for anyone. Matchmake, Converse & Play!",
+		images: [
+			{
+				url: 'https://gamebuddies.vercel.app/graphics/meta.png',
+				width: 800,
+				height: 600,
+				alt: 'Gamebuddies | By Luceforge Studio'
+			}
+		],
+		siteName: 'Your Website'
+	}}
+	twitter={{
+		handle: '@AdanAyaz1',
+		site: '@adanayaz',
+		cardType: 'summary_large_image',
+		title: 'Gamebuddies | By Luceforge Studio',
+		description:
+			"Connect with other gamebuddies, team up on the spot and start dealing with business! Don't wait for anyone. Matchmake, Converse & Play!",
+		image: 'https://gamebuddies.vercel.app/graphics/meta.png',
+		imageAlt: 'Gamebuddies | By Luceforge Studio'
+	}}
+/>
 <div class="min-h-screen">
 	<FirstSection />
 	<SecondSection />
